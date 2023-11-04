e1420474348062dcd2a9c02ea5093d73c55b3e1c
PhamHien1610 <149572101+PhamHien1610@users.noreply.github.com>
2023-11-04 15:41:27 +0700

    Create demo01_p5.php

---
+++ demo01_p5.php
@@ -0,0 +1,5 @@
+<?php
+echo "V/Git branch <br/>";
+echo "Các Branch (nhánh) đại diện cho các phiên bản cụ thể của một kho lưu trữ tách ra từ project chính của bạn. <br/>";
+echo "Branch cho phép bạn theo dõi các thay đổi thử nghiệm bạn thực hiện đối với kho lưu trữ và có thể hoàn nguyên về các phiên bản cũ hơn.";
+?>
