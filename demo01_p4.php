11379892494d0d07049f720cda3e0f1e19aeb000
Hien <phamhien16102003@gmail.com>
2023-11-04 20:26:27 +0700

    Add new file demo01_p4.php

---
+++ demo01_p4.php
@@ -0,0 +1,15 @@
+<?php
+echo "4. Các lệnh Git cơ bản <br/>";
+echo "4.1.Git init <br/>";
+echo "Tác dụng : Khởi tạo 1 git repository 1 project mới hoặc đã có. <br/>";
+echo "4.2. Git status <br/>";
+echo "Tác dụng: Để check trạng thái của những file bạn đã thay đổi trong thư mục làm việc <br/>";
+echo "4.3. Git commit <br/>";
+echo "Tác dụng: commit nghĩa là một action để Git lưu lại một snapshot của các sự thay đổi trong thư mục làm việc. Và các tập tin, thư mục được thay đổi đã phải nằm trong Staging Area. Mỗi lần commit nó sẽ được lưu lại lịch sử chỉnh sửa của code kèm theo tên và địa chỉ email của người commit. Ngoài ra trong Git bạn cũng có thể khôi phục lại tập tin trong lịch sử commit của nó để chia cho một branch khác, vì vậy bạn sẽ dễ dàng khôi phục lại các thay đổi trước đó. <br/>";
+echo "4.4. git push/git pull <br/>";
+echo "Tác dụng: Push hoặc Pull các thay đổi đến remote. Nếu bạn đã added và committed các thay đổi và bạn muốn đẩy nó lên hoặc remote của bạn đã update và bạn apply tất cả thay đổi đó trên code của mình. <br/>";
+echo "4.5.Git branch <br/>";
+echo "Tác dụng: liệt kê tất cả các branch (nhánh). <br/>";
+echo "4.6. git add <br/> ";
+echo "Tác dụng: Để đưa một tập tin vào Staging Area";
+?>
